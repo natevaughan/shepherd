4b85363739256d3c8d5c1dd7fa5a9a7af4fd317a
rafa <rafael@rocketmiles.com>
2018-11-16 09:27:14 -0600

    adding pen for the sheep with movving gate triggered by mouse click

---
+++ Assets/Scripts/GateController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateController : MonoBehaviour {
+
+    private bool animate = false;
+    private bool open = true;
+    private int xDistance = 0;
+    private int xDistanceMin = 0;
+    private int xDistanceMax = 60;
+    private int speed = 10;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (animate)
+        {
+            if (!open && xDistance >= xDistanceMin)
+            {
+                transform.Translate(Vector3.left * speed * Time.deltaTime);
+                xDistance--;
+            }
+            else if (open && xDistance <= xDistanceMax)
+            {
+                transform.Translate(Vector3.right * speed * Time.deltaTime);
+                xDistance++;
+            }
+            if (xDistance == xDistanceMin || xDistance == xDistanceMax)
+            {
+                open = !open;
+                animate = !animate;
+            }
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        animate = !animate;
+    }
+}
---
+++ Assets/Scripts/SheepController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepController : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody sheep;
+
+    // Use this for initialization
+    void Start () {
+        sheep = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        float moveHorizontal = 0;
+        float moveVertical = 1;
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        sheep.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Change Direction"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
